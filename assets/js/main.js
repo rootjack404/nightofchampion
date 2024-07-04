9ecbffd700ee9f22c53732ff8d2657060e99eff6
rootjack404 <jakirahman04@gmail.com>
2024-07-04 12:51:14 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,7 +77,7 @@ sr.reveal(`.home__header, .section__title`,{delay: 600})
 sr.reveal(`.home__footer`,{delay: 700})
 sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
 
-sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
+sr.reveal(`.sponsor__img, .sponsor__wideimg, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
 sr.reveal(`.specs__data, .specsr__data, .discount__animate`,{origin: 'left', interval: 100})
 sr.reveal(`.specs__img, .specsr__img, .discount__img`,{origin: 'right'})
 sr.reveal(`.case__img`,{origin: 'top'})
